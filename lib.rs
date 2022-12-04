4b0806fc6603d3fc8febd8acb49089cec04f904d
jazzysnake <dotya77@gmail.com>
2022-12-04 20:28:48 +0100

    swap proposals creation and countering

--- lib.rs
+++ lib.rs
@@ -4,6 +4,8 @@ use ink_lang as ink;
 
 #[ink::contract]
 mod course_reg {
+    //use ink_env::debug_println;
+    use ink_env::hash;
     use ink_storage::Mapping;
     use ink_prelude::vec::Vec;
     use ink_storage::traits::{SpreadAllocate, PackedLayout, SpreadLayout};
@@ -25,7 +27,7 @@ mod course_reg {
     }
 
     /// A course registration token
-    #[derive(PackedLayout, SpreadLayout, scale::Encode, scale::Decode)]
+    #[derive(PackedLayout, SpreadLayout, scale::Encode, scale::Decode, PartialEq, Debug)]
     #[cfg_attr(feature = "std", derive(::scale_info::TypeInfo))]
     pub struct CourseRegistration {
         /// the owner of the token
@@ -55,7 +57,7 @@ mod course_reg {
         /// the courses created by the teachers <CourseId, Course>
         courses: Mapping<[u8; 32], Course>,
         /// the proposed swaps <swapId, swapProposal>
-        swaps: Mapping<[u8; 32], CourseRegistrationSwapProposal>,
+        swaps: Mapping<[u8; 32], Vec<CourseRegistrationSwapProposal>>,
         /// the owned registration tokens <owner, tokens>
         registrations: Mapping<AccountId, Vec<CourseRegistration>>,
     }
@@ -65,6 +67,12 @@ mod course_reg {
     pub enum Error {
        InsufficientPermissions,
        NonexistentCourse,
+       CourseCapacityFull,
+       AlreadyRegistered,
+       NoRegistrations,
+       CourseAlreadyStarted,
+       NoSwappableRegistrations,
+       NoProposedSwap,
     }
 
     impl CourseReg {
@@ -128,7 +136,6 @@ mod course_reg {
             self.school_members.get(&account).unwrap_or(false)
         }
 
-
         /// Creates a university course
         #[ink(message)]
         pub fn create_course(&mut self,
@@ -150,6 +157,66 @@ mod course_reg {
             return Ok(())
         }
 
+        /// registers the caller to the university course
+        ///
+        /// the caller must be an admitted member and can't
+        /// register to the same course multiple times
+        #[ink(message)]
+        pub fn register_to_course(&mut self, course_id: [u8; 32]) -> Result<(), Error> {
+            let caller = Self::env().caller();
+            if !self.is_school_member_inner(caller) {
+                return Err(Error::InsufficientPermissions);
+            }
+            if !self.courses.contains(course_id) {
+                return Err(Error::NonexistentCourse);
+            }
+            let mut course = self.courses.get(course_id).unwrap();
+            let alerady_registered = course.registrations.len();
+            if alerady_registered >= course.capacity.try_into().unwrap() {
+                return Err(Error::CourseCapacityFull);
+            }
+            if course.registrations.contains(&caller) {
+                return Err(Error::AlreadyRegistered);
+            }
+            let current_time = Self::env().block_timestamp();
+            if course.start_date <= current_time {
+                return Err(Error::CourseAlreadyStarted);
+            }
+            course.registrations.push(caller);
+            self.courses.insert(&course_id, &course);
+            self.add_registration(course_id);
+            Ok(())
+        }
+
+        /// creates a CourseRegistration token for the course with course_id
+        /// and the caller becomes the owner of the token
+        fn add_registration(&mut self, course_id: [u8;32]) {
+            let caller = Self::env().caller();
+            let course_reg = CourseRegistration { owner: caller, course_id};
+            if !self.registrations.contains(caller) {
+                let mut registrations = Vec::new();
+                registrations.push(course_reg);
+                self.registrations.insert(&caller, &registrations);
+                return;
+            }
+            let mut registrations = self.registrations.get(caller).unwrap();
+            registrations.push(course_reg);
+            self.registrations.insert(&caller, &registrations);
+        }
+
+        /// Gets the caller's CourseRegistration tokens
+        #[ink(message)]
+        pub fn get_own_registrations(&self) -> Result<Vec<CourseRegistration>,Error> {
+            let caller = Self::env().caller();
+            if !self.registrations.contains(caller) || 
+                self.registrations.get(caller).unwrap().len() == 0 {
+                return Err(Error::NoRegistrations);
+            }
+            let registrations = self.registrations.get(caller).unwrap();
+            Ok(registrations)
+        }
+
+        /// Gets the info of a university course
         #[ink(message)]
         pub fn get_course_info(&self, course_id: [u8; 32]) -> Result<Course,Error> {
             if !self.courses.contains(&course_id){
@@ -158,11 +225,108 @@ mod course_reg {
             Ok(self.courses.get(course_id).unwrap())
         }
 
+        /// Proposes a course registration swap
+        #[ink(message)]
+        pub fn propose_swap(&mut self, course_id: [u8; 32]) -> Result<(),Error> {
+            let caller = Self::env().caller();
+            if !self.registrations.contains(caller) {
+                return Err(Error::NoSwappableRegistrations);
+            }
+            let mut registrations = self.registrations.get(caller).unwrap();
+            let course = registrations.iter().position(|x| x.course_id == course_id);
+            if course.is_none() {
+                return Err(Error::NoSwappableRegistrations);
+            }
+            let course = course.unwrap();
+            let course = registrations.remove(course);
+            self.registrations.insert(&caller, &registrations);
+
+
+            let proposal = CourseRegistrationSwapProposal {
+                offer: course,
+                counter_offers: Vec::default(),
+            };
+            self.add_proposal(course_id, proposal);
+            Ok(())
+        }
+
+        /// creates a Swap proposal token for the given course
+        /// and places it in the list of proposals for that course
+        fn add_proposal(&mut self,course_id: [u8; 32], proposal: CourseRegistrationSwapProposal) {
+            if !self.swaps.contains(course_id) {
+                let mut swaps = Vec::new();
+                swaps.push(proposal);
+                self.swaps.insert(&course_id, &swaps);
+                return;
+            }
+            let mut swaps = self.swaps.get(course_id).unwrap();
+            swaps.push(proposal);
+            self.swaps.insert(&course_id, &swaps);
+        }
+
+        /// retrieve swap proposals for a given course_id
+        #[ink(message)]
+        pub fn get_proposed_swaps(&self, course_id: [u8; 32]) -> Result<Vec<CourseRegistrationSwapProposal>, Error> {
+            if !self.swaps.contains(course_id) {
+                return Err(Error::NoProposedSwap);
+            }
+            Ok(self.swaps.get(course_id).unwrap())
+        }
+
+        /// Place a counter offer on a swap proposal
+        #[ink(message)]
+        pub fn counter_swap_proposal(&mut self, 
+                                     course_id: [u8;32],
+                                     offerer: AccountId,
+                                     counter_course_id: [u8; 32]) -> Result<(), Error> {
+            let caller = Self::env().caller();
+            let caller_regs = self.get_own_registrations();
+
+            if caller_regs.is_err() {
+                return Err(Error::NoProposedSwap);
+            }
+
+            let mut caller_regs = caller_regs.unwrap();
+            let exchange_course = caller_regs.iter().position(|reg| reg.course_id == counter_course_id);
+
+            if exchange_course.is_none(){
+                return Err(Error::NoProposedSwap);
+            }
+            let exchange_course = exchange_course.unwrap();
+            let exchange_course = caller_regs.remove(exchange_course);
+            self.registrations.insert(&caller, &caller_regs);
+
+            if !self.swaps.contains(course_id) {
+                return Err(Error::NoProposedSwap);
+            }
+
+            let proposals = self.swaps.get(course_id).unwrap();
+            let mut found = false;
+
+            for mut proposal in proposals {
+                if proposal.offer.owner == offerer {
+                    found = true;
+                    proposal.counter_offers.push(exchange_course);
+                    break;
+                }
+            }
+            if !found {
+                return Err(Error::NoProposedSwap);
+            }
+
+            Ok(())
+        }
 
         /// returns true if the caller is the owner of the contract
         fn is_owner(&self) -> bool {
             let caller = Self::env().caller();
             return caller == self.owner;
+        }
+        /// returns teh Keccak256 hash of the input bytes
+        pub fn hash_keccak_256(input: &[u8]) -> [u8; 32] {
+            let mut output = <hash::Keccak256 as hash::HashOutput>::Type::default();
+            ink_env::hash_bytes::<hash::Keccak256>(input, &mut output);
+            output
         }
     }
 
@@ -173,7 +337,6 @@ mod course_reg {
 
         use ink_lang as ink;
         use ink_env;
-        use ink_env::hash;
 
         fn set_next_caller(caller: AccountId) {
             ink_env::test::set_caller::<ink_env::DefaultEnvironment>(caller);
@@ -232,9 +395,111 @@ mod course_reg {
             assert_eq!(course_reg.admit_as_teacher(teacher), Ok(()));
             assert_eq!(course_reg.is_school_member(teacher), true);
             assert_eq!(course_reg.is_teacher(teacher), true);
+            set_next_caller(teacher);
             assert_eq!(course_reg.create_course(course_id, course_cap, start_time), Ok(()));
 
             assert_ne!(course_reg.get_course_info(course_id), Err(Error::NonexistentCourse));
+        }
+
+        /// Course registration test
+        #[ink::test]
+        fn course_registration() {
+            let owner = AccountId::from([0x0;32]);
+            set_next_caller(owner);
+            let mut course_reg = CourseReg::new(owner);
+            let teacher = AccountId::from([0x1; 32]);
+            let student = AccountId::from([0x2; 32]);
+            let course_name = "test_course".as_bytes();
+            let course_id = hash_keccak_256(course_name);
+            let course_cap:u32 = 10;
+            let start_time = get_current_time();
+
+            assert_eq!(course_reg.admit_as_teacher(teacher), Ok(()));
+            assert_eq!(course_reg.admit_as_student(student), Ok(()));
+            assert_eq!(course_reg.is_school_member(teacher), true);
+            assert_eq!(course_reg.is_teacher(teacher), true);
+            assert_eq!(course_reg.is_school_member(student), true);
+            set_next_caller(teacher);
+            assert_eq!(course_reg.create_course(course_id, course_cap, start_time), Ok(()));
+            assert_ne!(course_reg.get_course_info(course_id), Err(Error::NonexistentCourse));
+            set_next_caller(student);
+
+            assert_eq!(course_reg.register_to_course(course_id),Ok(()));
+            assert_ne!(course_reg.get_own_registrations(), Err(Error::NoRegistrations));
+        }
+
+        /// Swap proposal creation test
+        #[ink::test]
+        fn swap_proposal_creation() {
+            let owner = AccountId::from([0x0;32]);
+            set_next_caller(owner);
+            let mut course_reg = CourseReg::new(owner);
+            let teacher = AccountId::from([0x1; 32]);
+            let student = AccountId::from([0x2; 32]);
+            let course_name = "test_course".as_bytes();
+            let course_id = hash_keccak_256(course_name);
+            let course_cap:u32 = 10;
+            let start_time = get_current_time();
+
+            assert_eq!(course_reg.admit_as_teacher(teacher), Ok(()));
+            assert_eq!(course_reg.admit_as_student(student), Ok(()));
+            assert_eq!(course_reg.is_school_member(teacher), true);
+            assert_eq!(course_reg.is_teacher(teacher), true);
+            assert_eq!(course_reg.is_school_member(student), true);
+            set_next_caller(teacher);
+            assert_eq!(course_reg.create_course(course_id, course_cap, start_time), Ok(()));
+            assert_ne!(course_reg.get_course_info(course_id), Err(Error::NonexistentCourse));
+            set_next_caller(student);
+            assert_eq!(course_reg.register_to_course(course_id), Ok(()));
+            assert_ne!(course_reg.get_own_registrations(), Err(Error::NoRegistrations));
+
+            assert_eq!(course_reg.propose_swap(course_id), Ok(()));
+            assert_eq!(course_reg.get_proposed_swaps(course_id).unwrap().len(),1); 
+            assert_eq!(course_reg.get_own_registrations(), Err(Error::NoRegistrations));
+        }
+        //fn advance_block() {
+        //    ink_env::test::advance_block::<ink_env::DefaultEnvironment>();
+        //}
+        /// Counter swap proposal test
+        #[ink::test]
+        fn swap_proposal_counter() {
+            let owner = AccountId::from([0x0;32]);
+            set_next_caller(owner);
+            let mut course_reg = CourseReg::new(owner);
+            let teacher = AccountId::from([0x1; 32]);
+            let student1 = AccountId::from([0x2; 32]);
+            let student2 = AccountId::from([0x3; 32]);
+            let course_name1 = "test_course1".as_bytes();
+            let course_id1 = hash_keccak_256(course_name1);
+            let course_name2 = "test_course2".as_bytes();
+            let course_id2 = hash_keccak_256(course_name2);
+            let course_cap:u32 = 10;
+            let start_time = get_current_time();
+
+            assert_eq!(course_reg.admit_as_teacher(teacher), Ok(()));
+            assert_eq!(course_reg.admit_as_student(student1), Ok(()));
+            assert_eq!(course_reg.admit_as_student(student2), Ok(()));
+            assert_eq!(course_reg.is_school_member(teacher), true);
+            assert_eq!(course_reg.is_teacher(teacher), true);
+            assert_eq!(course_reg.is_school_member(student1), true);
+            assert_eq!(course_reg.is_school_member(student2), true);
+            set_next_caller(teacher);
+            assert_eq!(course_reg.create_course(course_id1, course_cap, start_time), Ok(()));
+            assert_ne!(course_reg.get_course_info(course_id1), Err(Error::NonexistentCourse));
+            assert_eq!(course_reg.create_course(course_id2, course_cap, start_time), Ok(()));
+            assert_ne!(course_reg.get_course_info(course_id2), Err(Error::NonexistentCourse));
+            set_next_caller(student1);
+            assert_eq!(course_reg.register_to_course(course_id1), Ok(()));
+            assert_ne!(course_reg.get_own_registrations(), Err(Error::NoRegistrations));
+            assert_eq!(course_reg.propose_swap(course_id1), Ok(()));
+            assert_eq!(course_reg.get_proposed_swaps(course_id1).unwrap().len(),1); 
+            set_next_caller(student2);
+            assert_eq!(course_reg.register_to_course(course_id2), Ok(()));
+            assert_ne!(course_reg.get_own_registrations(), Err(Error::NoRegistrations));
+
+            assert_eq!(course_reg.counter_swap_proposal(course_id1, student1, course_id2), Ok(()));  
+            set_next_caller(student2);
+            assert_eq!(course_reg.get_own_registrations(), Err(Error::NoRegistrations));
         }
     }
 }
